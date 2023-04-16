452e3e03cc82b1bb88daec019ddeb949f8e4db0b
JackMartinDev <jackmartin.dev@gmail.com>
2023-04-16 22:35:07 +0900

    Added Card UI component, BookList and Books css files

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -1,11 +1,15 @@
+import "./Book.css";
+
 const Book = (props) => {
   return (
-    <div>
-      <img src={props.img}></img>
-      <h3>Title: {props.title}</h3>
-      <h3>Author: {props.author}</h3>
-      <h3>Rating: {props.rating}</h3>
-    </div>
+    <li className="book">
+      <div>
+        <img src={props.img}></img>
+        <h3>Title: {props.title}</h3>
+        <h3>Author: {props.author}</h3>
+        <h3>Rating: {props.rating}</h3>
+      </div>
+    </li>
   );
 };
 
--- src/components/Books/BookList.js
+++ src/components/Books/BookList.js
@@ -1,38 +1,46 @@
 import { useState } from "react";
 import Book from "./Book";
+import "./BookList.css";
+import Card from "../../UI/Card";
 
 const DUMMY_ARRAY = [
   {
+    id: "1",
     title: "Haruhi 1",
     author: "John",
     rating: "2",
     img: "http://books.google.com/books/content?id=DZQsE3HYIvQC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
   },
   {
+    id: "2",
     title: "Haruhi 2",
     author: "John",
     rating: "2",
     img: "http://books.google.com/books/content?id=a_c-BAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
   },
   {
+    id: "3",
     title: "Haruhi 3",
     author: "John",
     rating: "2",
     img: "http://books.google.com/books/content?id=Dq2XJdDXuOIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
   },
   {
+    id: "4",
     title: "Haruhi 4",
     author: "John",
     rating: "2",
     img: "http://books.google.com/books/content?id=kvmL5Ad5josC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
   },
   {
+    id: "5",
     title: "Haruhi 5",
     author: "John",
     rating: "2",
     img: "http://books.google.com/books/content?id=mDrkgaOmjZYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
   },
   {
+    id: "6",
     title: "Haruhi 6",
     author: "John",
     rating: "2",
@@ -51,17 +59,22 @@ const BooksList = (props) => {
   if (filteredArray.length === 0) isEmpty = true;
 
   //TODO ADD KEY
+
+  const booksList = filteredArray.map((book) => (
+    <Book
+      id={book.id}
+      img={book.img}
+      title={book.title}
+      author={book.author}
+      rating={book.rating}
+    ></Book>
+  ));
   return (
-    <div>
-      {isEmpty && <p>No books found</p>}
-      {filteredArray.map((book) => (
-        <Book
-          img={book.img}
-          title={book.title}
-          author={book.author}
-          rating={book.rating}
-        ></Book>
-      ))}
+    <div className="books">
+      <Card>
+        {isEmpty && <p>No books found</p>}
+        <ul>{booksList}</ul>
+      </Card>
     </div>
   );
 };
