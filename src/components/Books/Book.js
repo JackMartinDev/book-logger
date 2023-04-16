df4afead23d072379602d1e31d222ac7d05350c8
JackMartinDev <jackmartin.dev@gmail.com>
2023-04-16 22:16:31 +0900

    Added image support

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -1,6 +1,7 @@
 const Book = (props) => {
   return (
     <div>
+      <img src={props.img}></img>
       <h3>Title: {props.title}</h3>
       <h3>Author: {props.author}</h3>
       <h3>Rating: {props.rating}</h3>
--- src/components/Books/BookList.js
+++ src/components/Books/BookList.js
@@ -1,17 +1,48 @@
 import { useState } from "react";
 import Book from "./Book";
 
+const DUMMY_ARRAY = [
+  {
+    title: "Haruhi 1",
+    author: "John",
+    rating: "2",
+    img: "http://books.google.com/books/content?id=DZQsE3HYIvQC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Haruhi 2",
+    author: "John",
+    rating: "2",
+    img: "http://books.google.com/books/content?id=a_c-BAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Haruhi 3",
+    author: "John",
+    rating: "2",
+    img: "http://books.google.com/books/content?id=Dq2XJdDXuOIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Haruhi 4",
+    author: "John",
+    rating: "2",
+    img: "http://books.google.com/books/content?id=kvmL5Ad5josC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Haruhi 5",
+    author: "John",
+    rating: "2",
+    img: "http://books.google.com/books/content?id=mDrkgaOmjZYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Haruhi 6",
+    author: "John",
+    rating: "2",
+    img: "http://books.google.com/books/content?id=tEuiXbsJofwC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+];
+
 const BooksList = (props) => {
   const [books, setBooks] = useState();
 
-  const DUMMY_ARRAY = [
-    { title: "Haruhi 1", author: "John", rating: "2" },
-    { title: "Haruhi 2", author: "John", rating: "2" },
-    { title: "Haruhi 3", author: "John", rating: "2" },
-    { title: "Haruhi 4", author: "John", rating: "2" },
-    { title: "Haruhi 5", author: "John", rating: "2" },
-    { title: "Haruhi 6", author: "John", rating: "2" },
-  ];
   const filteredArray = DUMMY_ARRAY.filter((book) =>
     book.title.includes(props.bookTitle)
   );
@@ -25,6 +56,7 @@ const BooksList = (props) => {
       {isEmpty && <p>No books found</p>}
       {filteredArray.map((book) => (
         <Book
+          img={book.img}
           title={book.title}
           author={book.author}
           rating={book.rating}
